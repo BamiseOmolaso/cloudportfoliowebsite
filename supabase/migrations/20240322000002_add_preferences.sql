64f9f9e37b05fb54d59fd333e581204c1ae206f1
Bamise Omolaso <davidbams3@gmail.com>
2025-11-24 08:21:28 +0100

    removed supabase

--- supabase/migrations/20240322000002_add_preferences.sql
+++
@@ -1,3 +0,0 @@
--- Add preferences column to newsletter_subscribers table
-ALTER TABLE newsletter_subscribers
-ADD COLUMN IF NOT EXISTS preferences JSONB DEFAULT '{"frequency": "weekly", "categories": []}'::jsonb; 
