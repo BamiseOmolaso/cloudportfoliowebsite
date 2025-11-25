40320e3d3bb370f1552d39460e2da004dd955d45
Bamise Omolaso <davidbams3@gmail.com>
2025-11-25 23:46:48 +0100

    Updated Docker file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Required for Docker deployment
+  output: 'standalone',
   reactStrictMode: true,
   images: {
     remotePatterns: [
